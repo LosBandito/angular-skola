714e070b87d1ec8a3d58502448b1af85b51fa255
LosBandito <91223061+LosBandito@users.noreply.github.com>
2022-11-07 09:05:05 +0100

    7 files modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { OneComponent } from './one.component/one.component';
+import { ThreeComponent } from './three.component/three.component';
+import { TwoComponent } from './two.component/two.component';
 
-const routes: Routes = []; // sets up routes constant where you define your routes
+const routes: Routes = [
+  { path: 'one', component: OneComponent },
+  { path: 'two', component: TwoComponent },
+  { path: 'three', component: ThreeComponent },
+];
 
 // configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
